f2270ded8ca1a473a1b42683dba80b39c89a8362
kapufskipauw <rosarhode@gmail.com>
2026-03-28 19:35:30 +0100

    add: Added content to kennisbank.blade.php

--- resources/views/kennisbank.blade.php
+++ resources/views/kennisbank.blade.php
@@ -1,7 +1,22 @@
 <x-layout
     title="kennisbank"
     heading="KENNISBANK">
-    <h2 class="text-introduction-title">
-        Hier moet een kennisbank komen
-    </h2>
+    <div class="introduction-container">
+        <div class="text-introduction-container">
+            <h2 class="text-introduction-title">
+                Leer meer over Ada Lovelace en vrouwen in tech
+            </h2>
+{{--            <p class="paragraph-styling">--}}
+{{--                <?php foreach($resources as $resource) :?>--}}
+{{--                <li>--}}
+{{--                        <?= $resource['title_id'] ?>--}}
+{{--                </li>--}}
+
+{{--                <?php endforeach ?>--}}
+{{--            </p>--}}
+        </div>
+        <div class="image-introduction-container">
+            <img class="image-general-styling" src="/images/Ada_Lovelace_daguerreotype_by_Antoine_Claudet_1843_-_cropped.png" alt="this is a placeholder image">
+        </div>
+    </div>
 </x-layout>
