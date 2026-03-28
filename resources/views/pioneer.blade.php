080016d0f6e7c04bf80f49d5eb6d201f64ac7f9c
kapufskipauw <rosarhode@gmail.com>
2026-03-28 19:35:49 +0100

    add: Added content to pioneer.blade.php

--- resources/views/pioneer.blade.php
+++ resources/views/pioneer.blade.php
@@ -1,7 +1,166 @@
 <x-layout
     title="Biografie"
     heading="BIOGRAFIE">
-    <h2 class="text-introduction-title">
-        Hier moet een pioneers biografie pagina komen
-    </h2>
+    <div class="biografie-sub-nav biografie-sub-nav-paragraph text-sm">
+        <div class="biografie-sub-nav-inner">
+            <li>
+                <a href="#biografie-sectie">
+                    Biografie
+                </a>
+            </li>
+            <li>
+                <a href="/view/pioneer#impact-sectie">
+                    Impact
+                </a>
+            </li>
+            <li>
+                <a href="/view/biografie#bronnen-sectie">
+                    Bronnen
+                </a>
+            </li>
+        </div>
+    </div>
+
+    <div
+        id="biografie-sectie"
+        class="introduction-container">
+        <div class="text-biografie-container">
+            <p class="paragraph-styling">
+                <a class="biografie-link-styling" target="_blank" aria-label="Lees meer over Ada Lovelace" title="Lees meer over Ada Lovelace" ​ href="https://en.wikipedia.org/wiki/Ada_Lovelace">
+                    Ada Lovelace
+                </a> werd geboren op 10 december 1815 in Piccadilly Terrace, Middlesex (nu: Londen) en overleed op 27 november 1852 in Marylebone, London. Ada was een Engelse wiskundige die samenwerkte met
+                <a class="biografie-link-styling" target="_blank" aria-label="Lees meer over Charles Babbage" title="Lees meer over Charles Babbage" href="https://www.britannica.com/biography/Charles-Babbage">
+                    Charles Babbage
+                </a>, voor zijn prototype digitale computer schreef zij een programma. Ze is benoemd tot de eerste
+                <a class="biografie-link-styling" target="_blank" aria-label="Lees meer over wat een computerprogrammeur is" title="Lees meer over wat een computerprogrammeur is" href="https://wikikids.nl/Programmeur">
+                    computerprogrammeur
+                </a>.
+                <br>
+                <br> Ada was de dochter van de bekende dichter
+                <a class="biografie-link-styling" target="_blank" aria-label="Lees meer over de vader van Ada Lovelace" title="Lees meer over de vader van Ada Lovelace" href="https://www.britannica.com/biography/Lord-Byron-poet">
+                    Heer Byron
+                </a> en
+                <a class="biografie-link-styling" aria-label="Lees meer over de moeder van Ada Lovelace" title="Lees meer over de moeder van Ada Lovelace" target="_blank" href="https://en.wikipedia.org/wiki/Lady_Byron">
+                    Annabella Milbanke
+                </a>, een vrouw met een sterke interesse in wiskunde en logica. Kort na de geboorte van Lovelace gaan haar ouders uit elkaar. Haar vader verliet Engeland voorgoed, dus ze leerde hem nooit persoonlijk kennen.
+                <br>
+                <br>Haar moeder stimuleerde haar in de exacte wetenschappen, naar horen en zeggen:'Om te voorkomen dat Ada, net als haar vader,
+                te emotioneel of “dromerig” zou worden.’ Ada bleek al jong talent te hebben voor wiskunde en natuurkunde.
+                Ze kreeg privéles door verschillende tutors, waaronder
+                <a class="biografie-link-styling" aria-label="Lees meer over de moeder van Ada Lovelace" target="_blank" title="Lees meer over Augustus De Morgan" href="https://nl.wikipedia.org/wiki/Augustus_De_Morgan">
+                    Augustus De Morgan
+                </a>, de eerste professor wiskunde aan de Universiteit van Londen. Op 8 juli 1835 trouwende ze met William King, de 8ste baron King, die in 1839 tot graaf werd benoemd, waardoor Ada de gravin van Lovelace werd.
+                <br>
+                <br> Lovelace raakte sinds 1833 geinteresseerd in Babbage’s machines nadat zij door
+                <a class="biografie-link-styling" aria-label="Leer meer over Mary Somerville" target="_blank" title="Leer meer over Mary Somerville" href="https://en.wikipedia.org/wiki/Mary_Somerville">
+                    Mary Somerville
+                </a> en een bekende wiskundige en wetenschapscommunicator werd voorgesteld aan Charles Babbage. Charles was de ontwerper van de
+                <a class="biografie-link-styling" aria-label="Lees meer over de analytical engine" target="_blank" title="Lees meer over de analytical engine" href="https://www.britannica.com/technology/Analytical-Engine">
+                    Analytical Engine
+                </a>, een mechanische machine die wordt beschouwd als een voorloper van de moderne computer.
+                <br>
+                <br> Ada’s grootste bijdrage kwam toen zij in 1843 een artikel van de Italiaanse ingenieur Luigi Menabrea vertaalde over Babbage’ machine.
+                <a class="biografie-link-styling" aria-label="Lees meer over de notities van Ada Lovelace" target="_blank" title="Lees meer over de notities van Ada Lovelace" href="https://old.maa.org/press/periodicals/convergence/mathematical-treasure-ada-lovelaces-notes-on-the-analytic-engine">
+                    Aan deze vertaling voegde zij uitgebreide eigen notities toe
+                </a>, die uiteindelijk veel langer waren dan het oorspronkelijke artikel. In deze notities beschreef zij een methode om de machine getallen te laten berekenen.
+                <br>
+                <br> Wat Ada bijzonder maakte, was haar visie: zij begreep dat computers in de toekomst niet alleen berekeningen zouden uitvoeren, maar ook muziek, tekst en afbeeldingen
+                zouden kunnen verwerken. Deze gedachte was haar tijd ver vooruit.
+                <br>
+                <br>Ada Lovelace overleed op slechts 36-jarige leeftijd aan kanker. Lange tijd bleef haar werk relatief onbekend, maar in de 20e eeuw kreeg zij postuum erkenning als pionier van de informatica.
+                Vandaag de dag wordt zij gezien als een inspiratiebron, vooral voor vrouwen in de technologie en wetenschap.
+            </p>
+        </div>
+    </div>
+
+    <div class="image-introduction-container">
+        <img class="image-general-styling image-biografie" src="http://blogs.bodleian.ox.ac.uk/adalovelace/wp-content/uploads/sites/163/2018/07/800px-AnalyticalMachine_Babbage_London.jpg" alt="this is a placeholder image">
+    </div>
+
+    <div class="introduction-container">
+        <div
+            class="text-erkenning-container"
+            id="impact-sectie">
+            <p class="paragraph-styling">
+                Tijdens haar leven kreeg
+                <a class="biografie-link-styling" target="_blank" aria-label="Lees meer over Ada Lovelace" title="Lees meer over Ada Lovelace" ​ href="https://en.wikipedia.org/wiki/Ada_Lovelace">
+                    Ada Lovelace
+                </a> nauwelijks erkenning voor haar werk. Dit had meerdere redenen. Om te beginnen werd de
+                <a class="biografie-link-styling" aria-label="Lees meer over de analytical engine" target="_blank" title="Lees meer over de analytical engine" href="https://www.britannica.com/technology/Analytical-Engine">
+                    Analytical Engine
+                </a>, waarvoor Ada het
+                <a class="biografie-link-styling" aria-label="Lees meer over algoritmes" target="_blank" title="Lees meer over algoritmes" href="https://www.mediawijs.be/nl/artikels/wat-een-algoritme-en-hoe-werkt-het">
+                    algoritme
+                </a> schreef, nooit gebouwd. Hierdoor bleef haar werk lange tijd theoretisch. Daarnaast was de wetenschap in de negentiende eeuw een mannendomein. Het werk van vrouwen werd vaak ondergebracht onder de paraplu van een man die vervolgens de erkenning van het werk kreeg.
+            </p>
+
+            <h2 class="header-two-styling">
+                1950s: Een centrale plek binnen de geschiedenis van de computerwetenschap.
+            </h2>
+            <p class="paragraph-styling">
+                In de jaren 1950 schreef
+                <a class="biografie-link-styling" aria-label="Lees meer over Alan turing" target="_blank" title="Lees meer over Alan Turing" href="https://en.wikipedia.org/wiki/Alan_Turing">
+                    Alan Turing
+                </a>, ook wel bekend als de vader van <a class="biografie-link-styling" aria-label="Lees meer over computerwetenschap" target="_blank" title="Lees meer over computerwetenschap" href="https://nl.wikipedia.org/wiki/Informatica">
+                    computerwetenschap
+                </a>, het artikel
+                <a class="biografie-link-styling" aria-label="Lees hier het artikel" target="_blank" title="Lees hier het artikel" href="https://courses.cs.umbc.edu/471/papers/turing.pdf">
+                    'Computing Machinery and Intelligence'
+                </a>, waarin hij de haalbaarheid van
+                <a class="biografie-link-styling" aria-label="Lees meer over kunstmatige intelligentie" target="_blank" title="Lees meer over kunstmatige intelligentie" href="https://nl.wikipedia.org/wiki/Kunstmatige_intelligentie">
+                    kunstmatige intelligentie
+                </a> onderzocht. Later stond het artikel bekend als de
+                <a class="biografie-link-styling" aria-label="Lees meer over de Turing test" target="_blank" title="Lees meer over de Turing test" href="https://nl.wikipedia.org/wiki/Turingtest">
+                    'Turing test'
+                </a>. In zijn artikel verwijs hij naar wat hij “Lady Lovelace’s objection” noemde. Lovelace’ stelling was dat de Alalytical Engine enkel taken kon uitvoren, en zelf niks nieuws kon uitbrengen. In zijn artikel gebruikte hij haar stelling als tegenargument in zijn betoog over denkende machines. Sindsdien kreeg haar werk een centrale plaats binnen de geschiedenis van de computerwetenschap.
+            </p>
+
+            <h2 class="header-two-styling">
+                1980s: programmeertaal Ada
+            </h2>
+            <p class="paragraph-styling">
+                In 1980 werd de programmeertaal vernoemd naar Ada waarmee haar nalatenschap een blijvende plek kreeg binnen de informatica. De
+                <a class="biografie-link-styling" aria-label="Lees meer over programmeertaal Ada" target="_blank" title="Lees meer over programmeertaal Ada" href="https://nl.wikipedia.org/wiki/Ada_(programmeertaal)">
+                    Programmeertaal Ada
+                </a> verving zo’n 450 verschillende programmeertalen die door het Armerikaanse leger werden gebruikt.
+            </p>
+
+            <h2 class="header-two-styling">
+                Ada Lovelace Day
+            </h2>
+            <p class="paragraph-styling">
+                Sinds oktober 2009 wordt daarnaast jaarlijks
+                <a class="biografie-link-styling" aria-label="Lees meer over Ada Lovelave Day" target="_blank" title="Lees meer over Ada Lovelave Day" href="https://findingada.com/">
+                    Ada Lovelace Day
+                </a> georganiseerd, een initiatief van activiste
+                <a class="biografie-link-styling" aria-label="Lees meer over Suw Charman-Anderson" target="_blank" title="Lees meer over Suw Charman-Anderson" href="https://findingada.com/about/meet-alds-founder/">
+                    Suw Charman-Anderson
+                </a>. Deze dag vindt plaats op de tweede dinsdag van oktober en is bedoeld om de prestaties van vrouwen in de wetenschap, technologie, engineering en wiskunde (STEM) zichtbaar te maken. In het STEM gebied zijn hun bijdragen historisch gezien vaak onderbelicht gebleven. Ada Lovelace Day wordt gevierd door middel van uiteenlopende activiteiten, zoals evenementen op scholen, in academische instellingen, bibliotheken en musea, met name in de Verenigde Staten en het Verenigd Koninkrijk. Onderdeel hiervan zijn onder andere Wikipedia-edit-a-thons, die gericht zijn op het vergroten van de zichtbaarheid van
+                <a class="biografie-link-styling" aria-label="Lees 'Hoe komt het dat meisjes hun passie verliezen in STEM?'" target="_blank" title="Lees 'Hoe komt het dat meisjes hun passie verliezen in STEM?'" href="https://pulse.microsoft.com/nl-be/making-a-difference-nl-be/education-nl-be/fa1-hoe-komt-het-dat-meisjes-hun-passie-verliezen-in-stem/">
+                    vrouwen in STEM
+                </a>, evenals filmvertoningen en paneldiscussies.
+            </p>
+            <button>
+                Lees verder over initiatieven voor vrouwen in tech
+            </button>
+        </div>
+        <div class="image-introduction-container">
+            <img class="image-general-styling" src="https://www.npg.org.uk/assets/image-cache/long/mw304536/Ada-Lovelace.add94a85.webp" alt="this is a placeholder image">
+        </div>
+    </div>
+
+    <div class="introduction-container">
+        <div
+            id="bronnen-sectie"
+            class="text-introduction-container">
+            <p class="paragraph-styling">
+{{--                <?php foreach ($resources as $resource) : ?>--}}
+{{--                <li>--}}
+{{--                        <?= $resource['title_id'] ?>--}}
+{{--                </li>--}}
+
+{{--                <?php endforeach ?>--}}
+            </p>
+        </div>
+    </div>
 </x-layout>
