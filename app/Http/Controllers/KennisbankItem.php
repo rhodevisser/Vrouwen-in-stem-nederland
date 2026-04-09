e5ed788e6168693b1c5fcc5c6074c8a70c54ca67
Rhodé <116293384+rhodevisser@users.noreply.github.com>
2026-04-09 22:12:15 +0200

    build: Created DB for kennisbank items (#2)
    Added table for kennisbank items, route and factory 
    
    This outputs the Kennisbank's list items from the DB to the Kennisbank page.

---
+++ app/Http/Controllers/KennisbankItem.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class KennisbankItem extends Controller
+{
+    /**
+     * Show a list of all items of the kennisbank
+     */
+    public function index(): View
+    {
+        $Kennisbank_items = KennisbankItem::select('select * from users where active = ?', [1]);
+
+        return view('kennisbank.index', ['kennisbankItems' => $Kennisbank_items]);
+    }
+}
